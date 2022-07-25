ab2fc6a8e19bf27a1fb2a911f972a832031a6427
Achal Chaudhary <84672144+zeroWillreborn@users.noreply.github.com>
2022-07-26 01:34:19 +0530

    Solved Passing for fair

---
+++ Passes-for-fair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n,k;
+	cin >> t;
+	while(t--){
+	    cin>>n>>k;
+	    (k>n) ?  cout<<"YES"<<endl : cout<<"NO"<<endl;
+	};
+	return 0;
+}
