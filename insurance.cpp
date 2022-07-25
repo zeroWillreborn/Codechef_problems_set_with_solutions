af23978cf8558d807751dfd72d452052b5cf472f
Achal Chaudhary <84672144+zeroWillreborn@users.noreply.github.com>
2022-07-26 01:43:27 +0530

    Solved Insurance problem

---
+++ insurance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    y<=x ? cout<<y<<endl : cout<<x<<endl;
+	};
+	return 0;
+};
